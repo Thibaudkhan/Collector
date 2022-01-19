f37809c903a26b61e039ef08659c6e17ccaeec12
Thibaud <roussel@abcportage.com>
2022-01-19 10:30:40 +0100

    commit db change (add user and password)

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
     connectionLimit: 10,
     host: process.env.MYSQL_HOST || "localhost",
     user: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PASSWORD || "",
+    password: process.env.MYSQL_PASSWORD || "root",
     database: process.env.MYSQL_DATABASE || "collector",
 });
 
